62314c5fc8c973bcc6f33fd78614b0a0834e8726
bteoutlook <bte@outlook.jp>
2022-02-07 22:40:27 +0900

    update

--- stockmarket.py
+++ stockmarket.py
@@ -74,10 +74,10 @@ start, end  = st.slider('シミュレーション期間',
 
 
 # 全期間データ
-df1=data.DataReader(invest.head(1),'stooq',start_original,end_original)
+df1=data.DataReader(invest[0],'stooq',start_original,end_original)
 df1=df1.iloc[::-1]
 date1=df1.index
-df2=data.DataReader(invest.tail(0),'stooq',start_original,end_original)
+df2=data.DataReader(invest[-1],'stooq',start_original,end_original)
 df2=df2.iloc[::-1]
 date2=df2.index
 
@@ -86,30 +86,30 @@ fig = plt.figure()
 plt.subplots_adjust(hspace=0.6)
 
 ax1 = fig.add_subplot(2,1,1)
-ax1.plot(date1, df1['Close'],label=invest.head(1),color='red')
+ax1.plot(date1, df1['Close'],label=invest[0],color='red')
 ax1.set_title('stock market')
-ax1.set_ylabel(invest.head(1)+'(USD)')
+ax1.set_ylabel(invest[0]+'(USD)')
 ax1.legend()
 ax1.grid()
 ax1.tick_params(labelsize=7)
 
 ax2 = ax1.twinx()
-ax2.plot(date2, df2['Close'],label=invest.tail(1),color='blue')
-ax2.set_ylabel(invest.tail(1)+'(USD)')
+ax2.plot(date2, df2['Close'],label=invest[-1],color='blue')
+ax2.set_ylabel(invest[-1]+'(USD)')
 ax2.legend(loc='center left')
 ax2.tick_params(labelsize=7)
 
 ax3 = fig.add_subplot(2,1,2)
-ax3.plot(date1, df1['Close'],label=invest.head(1),color='red')
+ax3.plot(date1, df1['Close'],label=invest[0],color='red')
 ax3.set_xlabel('date')
-ax3.set_ylabel(invest.head(1)+'(USD)')
+ax3.set_ylabel(invest[0]+'(USD)')
 ax3.legend()
 ax3.grid()
 ax3.tick_params(labelsize=7)
 
 ax4 = ax3.twinx()
-ax4.set_ylabel(invest.tail(1)+'(USD)')
-ax4.plot(date2, df2['Close'],label=invest.tail(1),color='blue')
+ax4.set_ylabel(invest[-1]+'(USD)')
+ax4.plot(date2, df2['Close'],label=invest[-1],color='blue')
 ax4.legend(loc='center left')
 ax4.tick_params(labelsize=7)
 
